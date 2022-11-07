d90362ffcec3932f3cbd16def5d168b0db58d49c
lentilek <laura.robinska@wp.pl>
2022-11-07 20:47:58 +0100

    Patch Manager
    Adding patch manager and sprites

---
+++ Witch's Garden/Assets/scripts/PlotManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotManager : MonoBehaviour
+{
+    bool isPlanted = false;
+    SpriteRenderer plant;
+    BoxCollider2D plantCollider;
+
+    public Sprite[] plantStages;
+    int plantStage = 0;
+    float timeBS = 2f;
+    float timer;
+
+    void Start()
+    {
+        plant = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        plantCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (isPlanted)
+        {
+            timer -= Time.deltaTime;
+            if(timer < 0 && plantStage < plantStages.Length - 1)
+            {
+                timer = timeBS;
+                plantStage++;
+                UpdatePlant();
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlanted)
+        {
+            if (plantStage == plantStages.Length - 1)
+            {
+                Harvest();
+            }
+        }
+        else
+        {
+            Plant();
+        }
+    }
+
+    void Harvest()
+    {
+        isPlanted = false;
+        plant.gameObject.SetActive(false);
+    }
+
+    void Plant()
+    {
+        isPlanted = true;
+        plantStage = 0;
+        UpdatePlant();
+        timer = timeBS;
+        plant.gameObject.SetActive(true);
+    }
+
+    void UpdatePlant()
+    {
+        plant.sprite = plantStages[plantStage];
+        plantCollider.size = plant.sprite.bounds.size;
+        plantCollider.offset = new Vector2(0, plant.bounds.size.y/2);
+    }
+}
