55475269903b7f8b74bd5d5704f7a6593bfcbc8c
lentilek <laura.robinska@wp.pl>
2023-02-03 14:39:49 +0100

    wybór narzędzi i nazwy

--- Assets/scripts/GardenManager.cs
+++ Assets/scripts/GardenManager.cs
@@ -17,10 +17,16 @@ public class GardenManager : MonoBehaviour
     public Color buyColor = Color.green;
     public Color cancelColor = Color.red;
 
+    public bool isSelecting = false;
+    public int selectedTool = 0;
+
+    public Image[] buttonsIMG;
+    public TextMeshProUGUI[] btnTxt;
+
     void Start()
     {
         moneyTxt.text = "$" + money;
-        ordersTxt.text = "Orders: " + numberOfOrders;
+        ordersTxt.text = "Zamówienia: " + numberOfOrders;
     }
 
     public void SelectPlant(PlantItem newPlant)
@@ -28,23 +34,59 @@ public class GardenManager : MonoBehaviour
         if(selectPlant == newPlant)
         {
             //Debug.Log("Deselected " + selectPlant.plant.plantName);
-            selectPlant.btnImage.color = buyColor;
-            selectPlant.btnTxt.text = "Buy";
-            selectPlant = null;
-            isPlanting = false;
+            CheckSelection();
         }
         else
         {
-            if(selectPlant!= null)
-            {
-                selectPlant.btnImage.color = buyColor;
-                selectPlant.btnTxt.text = "Buy";
-            }
+            CheckSelection();
             selectPlant= newPlant;
             selectPlant.btnImage.color = cancelColor;
-            selectPlant.btnTxt.text = "Cancel";
+            selectPlant.btnTxt.text = "ODZNACZ";
             //Debug.Log("Selected " + selectPlant.plant.plantName);
             isPlanting = true;
+        }
+    }
+
+    public void SelectTool(int toolNumber)
+    {
+        if(toolNumber == selectedTool)
+        {
+            //deselect
+            CheckSelection();
+        }
+        else
+        {
+            //select
+            CheckSelection();
+            isSelecting= true;
+            selectedTool= toolNumber;
+            //buttonsIMG[toolNumber - 1].sprite = selectedButton;
+            buttonsIMG[selectedTool - 1].color = cancelColor;
+            btnTxt[selectedTool - 1].text = "ODZNACZ";
+        }
+    }
+
+    void CheckSelection()
+    {
+        if(isPlanting)
+        {
+            isPlanting= false;
+            if(selectPlant !=null) { 
+                selectPlant.btnImage.color = buyColor;
+                selectPlant.btnTxt.text = "KUP";
+                selectPlant = null;
+            }
+        }
+        if(isSelecting)
+        {
+            if(selectedTool > 0)
+            {
+                //buttonsIMG[selectedTool - 1].sprite = normalButton;
+                buttonsIMG[selectedTool-1].color = buyColor;
+                btnTxt[selectedTool - 1].text = "WYBIERZ";
+            }
+            isSelecting= false;
+            selectedTool = 0;
         }
     }
 
@@ -56,6 +98,6 @@ public class GardenManager : MonoBehaviour
     public void OrdersIncrease()
     {
         numberOfOrders++;
-        ordersTxt.text = "Orders: " + numberOfOrders;
+        ordersTxt.text = "Zamówienia: " + numberOfOrders;
     }
 }
